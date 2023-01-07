b36d75c8834f178744ed78d01cce2f7dfa3b3519
Arthur Dias <arthursantos01@gmail.com>
2023-01-07 17:24:41 -0300

    fix for upstash kafka secrets

--- src/infra/messaging/kafka/kafka-consumer.service.ts
+++ src/infra/messaging/kafka/kafka-consumer.service.ts
@@ -7,11 +7,11 @@ export class KafkaConsumerService extends ServerKafka implements OnModuleDestroy
     super({
       client: {
         clientId: 'notifications',
-        brokers: ['sunny-terrapin-13359-us1-kafka.upstash.io:9092'],
+        brokers: [process.env.KAFKA_UPSTASH_BROKER_URL!],
         sasl: {
           mechanism: 'scram-sha-256',
-          username: 'c3VubnktdGVycmFwaW4tMTMzNTkkeNQn_fvXxWTKXEK9u4Mh9WSHCP_OTnFppW4',
-          password: '9353f1e0bfec47a98cbec5d95023d528',
+          username: process.env.KAFKA_UPSTASH_USERNAME!,
+          password: process.env.KAFKA_UPSTASH_PASSWORD!,
         },
         ssl: true,
       }
